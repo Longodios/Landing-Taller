2847f8b02b286b3a3e9c1ae06352a176f19e2c5e
Longodios <tusaromix@gmail.com>
2024-11-22 15:54:01 +0100

    Avances en componentes de Carrusel,Footer  y Contacto

---
+++ src/components/Carrusel.jsx
@@ -0,0 +1,25 @@
+import react from '@astrojs/react';
+import imagenTestimonio from "/src/utils/imagenTestimonio.webp";
+
+
+
+const Carrusel = () => {
+    
+    return(
+        <>
+        <h2 className="text-white text-center font-semibold text-4xl mt-5">Testimonios </h2>
+        <section className="flex flex-col justify-center items-center mt-10 bg-black w-full">
+            <div className=" w-42 border-3 border-white rounded-full  h-42 absolute">
+                   <img src="/src/utils/imagenTestimonio.webp" alt="imagenTestimonio1" className="w-full object-cover rounded-full" />
+            </div>
+            <article className="w-80 h-[530px] border-1 border-white rounded-xl bg-tallerAzulMarino ">
+                
+
+            </article>
+        </section>   
+        </>
+    )
+}
+
+
+export default Carrusel;
