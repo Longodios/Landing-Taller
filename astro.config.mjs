6803522749f16893deb2feebfe7377e6b69f33a9
Longodios <tusaromix@gmail.com>
2024-11-25 16:18:29 +0100

    Nuevas dependencias añadidaS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  
   integrations: [
     tailwind(),
     react(), // React debe ir aquí como una integración
   ],
+
+  output: 'server',
+  adapter: vercel(),
 });
